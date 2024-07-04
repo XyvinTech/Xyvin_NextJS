2e19fb1b63f793f42c85b6149fadc3e445ae4125
Abhijith B <babhijith58@gmail.com>
2024-07-04 14:08:48 +0000

    dark mode removed

--- src/layouts/Wrapper.tsx
+++ src/layouts/Wrapper.tsx
@@ -2,17 +2,20 @@
 "use client"
 
 import React from 'react';
-import DarkLight from '@/components/common/DarkLight';
+// import DarkLight from '@/components/common/DarkLight';
 import ScrollToTop from '@/components/common/ScrollToTop';
+// import Sticky from '@/components/sticky/Sticky';
 
 const Wrapper = ({ children }: any) => {
 
   return (
-    <>
+
+      <>
       <ScrollToTop />
-      <DarkLight />
+      {/* <DarkLight /> */}
+      {/* <Sticky /></> */}
       {children}
-    </>
+ </>
   );
 };
 
