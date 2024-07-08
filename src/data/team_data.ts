ff83fb6b120351701d0269ec686b63b5027e5f54
Abhijith B <babhijith58@gmail.com>
2024-07-08 14:36:15 +0530

    team images added

--- src/data/team_data.ts
+++ src/data/team_data.ts
@@ -1,9 +1,9 @@
-import team_avatar_1 from "@/assets/img/team/team_1.png";
-import team_avatar_2 from "@/assets/img/team/team_2.png";
-import team_avatar_3 from "@/assets/img/team/team_3.png";
-import team_avatar_4 from "@/assets/img/team/team_4.png";
-import team_avatar_5 from "@/assets/img/team/team_5.png";
-import team_avatar_6 from "@/assets/img/teamsimg3.png";
+import team_avatar_1 from "@/assets/img/team/Team_1.png";
+import team_avatar_2 from "@/assets/img/team/Team_2.png";
+import team_avatar_3 from "@/assets/img/team/Team_3.png";
+import team_avatar_4 from "@/assets/img/team/Team_4.png";
+import team_avatar_5 from "@/assets/img/team/Team_5.png";
+import team_avatar_6 from "@/assets/img/teamsimg2.jpg";
 import team_avatar_7 from "@/assets/img/teamsimg7.jpg";
 import team_avatar_8 from "@/assets/img/teamsimg8.jpg";
 
